fdd5b7e5754a9cf818a61549f6462ae5c22bb56f
Andras Fuge <andras_fuge@epam.com>
2022-02-15 08:47:39 +0100

    add extra wait

--- src/main/java/driver/DriverWaits.java
+++ src/main/java/driver/DriverWaits.java
@@ -1,9 +1,12 @@
 package driver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.function.Function;
 
 import static driver.DriverManager.*;
 
@@ -175,5 +178,24 @@ public final class DriverWaits {
      */
     public static void waitForElementToBeClickable(WebElement element, long timeout) {
         getDefaultWait(timeout).until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    /**
+     * Waits for an ExpectedCondition to be true.
+     *
+     * @param isTrue The specified ExpectedCondition to meet.
+     */
+    public static <T> void waitForConditionToMeet(ExpectedCondition<T> isTrue){
+        waitForConditionToMeet(isTrue, getImplicitWaitTimeout());
+    }
+
+    /**
+     * Waits for an ExpectedCondition to be true.
+     *
+     * @param isTrue The specified ExpectedCondition to meet.
+     * @param timeout The timeout duration in seconds.
+     */
+    public static <T> void waitForConditionToMeet(ExpectedCondition<T> isTrue, long timeout){
+        getDefaultWait(timeout).until(isTrue);
     }
 }
