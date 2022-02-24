2c53bf7cae6145b7c7b502d27ba87ca59a90ce98
Andras Fuge <andras_fuge@epam.com>
2022-02-24 11:14:01 +0100

    update dependency versions

--- src/main/java/driver/DriverMethods.java
+++ src/main/java/driver/DriverMethods.java
@@ -1,7 +1,7 @@
 package driver;
 
-import cucumber.api.Scenario;
 import enums.MouseEvent;
+import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
@@ -440,7 +440,7 @@ public class DriverMethods {
     }
 
     private static void writeScreenshotToScenario(Scenario scenario, byte[] screenshot) {
-        scenario.embed(screenshot, "image/png");
+        scenario.attach(screenshot, "image/png", "screenshot");
     }
 
     /**
